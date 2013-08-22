a575494462f3e8e414ab2a5fc10634a4dd62d7d2
ChicagoBusinessIntelligence2 <ChicagoBusinessIntelligence2@live.com>
2013-08-22 13:34:19 -0500

    SEO changes

--- app/models/Rentstatus.php
+++ app/models/Rentstatus.php
@@ -3,11 +3,9 @@
 
 class Rentstatus extends Eloquent {
 	
-    protected $guarded = array();
+protected $guarded = array();
 
-    public static $rules = array();
-
-
+public static $rules = array();
 
 public function rental()
 {
