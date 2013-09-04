342162759d23eccc5b642c2687e503121eb93de3
ChicagoBusinessIntelligence1 <svitlana.shepitsena@chicagobusinessintelligence.net>
2013-09-04 10:46:56 -0500

    one stop service picture edit

--- app/views/search/onehouse.blade.php
+++ app/views/search/onehouse.blade.php
@@ -7,48 +7,63 @@
 	<div class="panel oneListing oneRentAgent">
 		<div class="row callAgentRent">
 
-
-@if(isset($house->maximgid))
-					<ul class="clearing-thumbs" data-clearing>
-						@for ($i =1; $i <= $house->maximgid; $i++)
-						<li class="clearing-featured-img"><a class="th" href="{{url('comp/img/images/'.$house->id.'/'.$i.'.jpg')}}">
-							<img width="100px" height="50px" src="{{url('comp/img/images/'.$house->id.'/'.$i.'.jpg')}}"></a>
+			<div class="row">
+				@if(isset($house->address))
+				<div class="large-9 columns">
+					<h1 class="oneHouseAddress label radius">{{$house->address}}</h1>
+					&nbsp &nbsp
+					<ul class="inline-list">
+							@if (isset($house->isforeclosed))
+						<li class="label radius foreclosureLabel">
+							Foreclose <br/>
 						</li>
-						@endfor
-					</ul>	
-					@endif
+							@endif
+							
+							@if (isset($house->shortsale))
+						<li class="label radius shortsaleLabel">
+							Short Sale<br/>
+						</li>
+							@endif
+							
+							@if (isset($house->isbankowned))
+						<li class="label radius bankowedLabel">
+							Bank Owned <br/>
+						</li>
+							@endif
+							 
+					</ul>
+				</div>
+				@endif
+				<div class="large-2 columns">
+					<span class="priceStyle right ">${{number_format($house->price)}}
+					</span>
+				</div>
+			</div>
+			<hr/>
+
+			@if(isset($house->maximgid))
+			<ul class="clearing-thumbs" data-clearing>
+				@for ($i =1; $i <= $house->maximgid; $i++)
+				<li class="clearing-featured-img"><a class="th" href="{{url('comp/img/images/'.$house->id.'/'.$i.'.jpg')}}">
+					<img width="100px" height="50px" src="{{url('comp/img/images/'.$house->id.'/'.$i.'.jpg')}}"></a>
+				</li>
+				@endfor
+			</ul>	
+			@endif
 
 
-			@if (isset($house->isforeclosed))
-			Foreclosed <br/>
-@endif
-
-			@if (isset($house->isbankowned))
-			Bank Owned <br/>
-@endif
 			
 		</div>
 		<!-- =callAgentRent ends here -->
-		<hr/>
 
-		<div class="row">
-			@if(isset($house->address))
-			<div class="large-9 columns">
-				<h1 class="oneHouseAddress label radius">{{$house->address}}</h1>
-				&nbsp &nbsp
-			</div>
-			@endif
-
-		</div>
-
-		
+		<br/>		
 		<div class="row">
 			<div class="large-12 columns listingDetails">
 
 				
 				<div class="row">
 					<div class="large-12 columns databaseParsing">
-					@if(isset($house->address))
+						@if(isset($house->address))
 						{{$house->description}}
 						@endif
 					</div>
