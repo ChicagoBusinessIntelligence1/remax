c0ce48f75e650870551ab5b0d7905a1cf9351267
ChicagoBusinessIntelligence1 <svitlana.shepitsena@chicagobusinessintelligence.net>
2013-09-05 15:49:21 -0500

    home page text editing

--- app/views/home/vw_home.blade.php
+++ app/views/home/vw_home.blade.php
@@ -10,7 +10,7 @@
 			<div class="panel radius">
 				<h1 class="welcome">Welcome to <br/>Remax 1st Class Realty</h1>
 					<hr/>
-					<p>Remax 1st Class Realty is a <em>trustful Real Estate Company</em> located in <em>Skokie, IL</em>. We offer a comprehensive service in real estate. We can help to buy, sell or manage residential property. Remax 1st Class Realty will also help to get the best mortgage conditions via our mortgage department as well as get all paper prepared by staff attorneys. We have successful experience in short sales and always have a great investment opportunities. Our main price range is from 35k till 500k, though we also have experience and listing in luxury market.</p>
+					<p>Remax 1st Class Realty is a <em>trustful Real Estate Company</em> located in <em>Skokie, IL</em>. We offer a comprehensive service in real estate. We can help to buy, sell or manage residential property. Remax 1st Class Realty will also help to find the right mortgage agent to get the best mortgage conditions as well as get all paper prepared by staff attorneys. We have successful experience in short sales and always have a great investment opportunities. Our main price range is from 35k till 500k, though we also have experience and listing in luxury market.</p>
 					<p>Our first line districts are Skokie, Morton Grove, Evanston, Niles, North Side Chicago, Ukrainian Village and neighboring areas.</p>
 				</div>
 			</div>
@@ -54,7 +54,7 @@
 					<hr/>
 					<ul class="ulWorkWith">
 						<li>
-							<p>we offer one stop service for home purchase. We have long term partnerships with major banks and can negotiate the best possible deal. We can move you to your new home during one week with the lowest mortgage rate. Our clients always have all papers signed on time with all details covered.</p>
+							<p>we offer one stop service for home selling or purchase. We have long term partnerships with major banks and can negotiate the best possible deal. We can help you find the right specialist to get the lowest mortgage rate and move you to your new home during one week. Our clients always have all papers signed on time with all details covered.</p>
 						</li>
 						<li>
 							<p>we have no hidden fees and as a Remax Representative, we are very flexible in commissions. We always prioritize the client interest and in some cases we work without commissions when they are obstacles to closing. Our main mission is client satisfaction and long term partnership.</p>
