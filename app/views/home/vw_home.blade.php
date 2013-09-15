1c07093d917b0c3cfba16ab09044a76f764e250d
ChicagoBusinessIntelligence1 <svitlana.shepitsena@chicagobusinessintelligence.net>
2013-09-15 13:03:38 -0500

    home page design

--- app/views/home/vw_home.blade.php
+++ app/views/home/vw_home.blade.php
@@ -9,31 +9,32 @@
 		<div class="large-12 columns about-us">
 			<div class="panel radius">
 				<h1 class="welcome">Welcome to <br/>Remax 1st Class Realty</h1>
-					<hr/>
-					<p>Remax 1st Class Realty is a <em>trustful Real Estate Company</em> located in <em>Skokie, IL</em>. We offer a comprehensive service in real estate. We can help to buy, sell or manage residential property. Remax 1st Class Realty will also help to find the right mortgage agent to get the best mortgage conditions as well as get all paper prepared by staff attorneys. We have successful experience in short sales and always have a great investment opportunities. Our main price range is from 35k till 500k, though we also have experience and listings in luxury market.</p>
-					<p>Our first line districts are: 
-					<ul>
-						<li>{{link_to_route('skokie-lists', 'Skokie');}}</li>
+				<hr/>
+				<p>Remax 1st Class Realty is a <em>trustful Real Estate Company</em> located in <em>Skokie, IL</em>. We offer a comprehensive service in real estate. We can help to buy, sell or manage residential property. Remax 1st Class Realty will also help to find the right mortgage agent to get the best mortgage conditions as well as get all paper prepared by staff attorneys. We have successful experience in short sales and always have a great investment opportunities. Our main price range is from 35k till 500k, though we also have experience and listings in luxury market.</p>
+				<p><em class="label secondary subheader">Our first line districts are:</em> 
+					<ul class="no-bullet inline-list ulFirstLineDistricts">
+						<li class="liFirstLineDistricts">{{link_to_route('skokie-lists', 'Skokie');}}</li>
 
-						<li>{{link_to_route('niles-lists', 'Niles');}}</li>
+						<li class="liFirstLineDistricts">{{link_to_route('niles-lists', 'Niles');}}</li>
 
-						<li>{{link_to_route('evanston-lists', 'Evanston');}}</li>
+						<li class="liFirstLineDistricts">{{link_to_route('evanston-lists', 'Evanston');}}</li>
 
-						<li>{{link_to_route('morton-grove-lists', 'Morton-Grove');}}</li>
+						<li class="liFirstLineDistricts">{{link_to_route('morton-grove-lists', 'Morton-Grove');}}</li>
 
-						<li>{{link_to_route('glenview-lists', 'Glenview');}}</li>
+						<li class="liFirstLineDistricts">{{link_to_route('glenview-lists', 'Glenview');}}</li>
 
-						<li>{{link_to_route('park-ridge-lists', 'Park-Ridge');}}</li>
+						<li class="liFirstLineDistricts">{{link_to_route('park-ridge-lists', 'Park-Ridge');}}</li>
 
-						<li>{{link_to_route('lincolnwood-lists', 'Lincolnwood');}}</li>
+						<li class="liFirstLineDistricts">{{link_to_route('lincolnwood-lists', 'Lincolnwood');}}</li>
 
-						<li>{{link_to_route('wilmette-lists', 'Wilmette');}}</li>
+						<li class="liFirstLineDistricts">{{link_to_route('wilmette-lists', 'Wilmette');}}</li>
 
-						<li>{{link_to_route('winnetka-lists', 'Winnetka');}}</li>
+						<li class="liFirstLineDistricts">{{link_to_route('winnetka-lists', 'Winnetka');}}</li>
 
-						<li>{{link_to_route('lake-forest-lists', 'Lake-Forest');}}</li>
-						<li>Northside of Chicago</li>
-						</div>
+						<li class="liFirstLineDistricts">{{link_to_route('lake-forest-lists', 'Lake-Forest');}}</li>
+						<li class="liFirstLineDistricts">Northside of Chicago</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 
@@ -46,28 +47,28 @@
 							<img src="{{url('comp/img/orbit/5.jpg')}}" alt="">
 							<div class="orbit-caption">Glenview, IL 60025</div>
 						</a>
-						</li>
-						<li> <a href="{{URL::to('search/1')}}">
-							<img src="{{url('comp/img/orbit/1.jpg')}}" alt="">
-							<div class="orbit-caption">Skokie, IL 60076   </div>
-						</a>
-						</li>
-						<li>
-						<a href="{{URL::to('search/16')}}">
-							<img src="{{url('comp/img/orbit/4.jpg')}}" alt="">
-							<div class="orbit-caption">Glenview, IL 60025</div>
-						</a>
-						</li>
-						<li><a href="{{URL::to('search/4')}}">
-							<img src="{{url('comp/img/orbit/2.jpg')}}" alt="">
-							<div class="orbit-caption">Morton Grove, IL</div>
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>	
-		</div>
+					</li>
+					<li> <a href="{{URL::to('search/1')}}">
+						<img src="{{url('comp/img/orbit/1.jpg')}}" alt="">
+						<div class="orbit-caption">Skokie, IL 60076   </div>
+					</a>
+				</li>
+				<li>
+					<a href="{{URL::to('search/16')}}">
+						<img src="{{url('comp/img/orbit/4.jpg')}}" alt="">
+						<div class="orbit-caption">Glenview, IL 60025</div>
+					</a>
+				</li>
+				<li><a href="{{URL::to('search/4')}}">
+					<img src="{{url('comp/img/orbit/2.jpg')}}" alt="">
+					<div class="orbit-caption">Morton Grove, IL</div>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>	
+</div>
 
-		</div>
-	@stop
+</div>
+@stop
 
