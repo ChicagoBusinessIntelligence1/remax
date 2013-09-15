56dad72b8f6db4ca0d443eff573cac0b7739ec92
ChicagoBusinessIntelligence1 <svitlana.shepitsena@chicagobusinessintelligence.net>
2013-09-15 14:37:13 -0500

    Agent listings

--- app/views/about/vw_hiring.blade.php
+++ app/views/about/vw_hiring.blade.php
@@ -6,28 +6,27 @@
 <div class="mainContent">
 	<div class="row">
 		<div class="large-12 columns menues">
-				
+
 			<h1 class="docs header">Join Remax First Class Realty Team</h1> <div class="row">
 			<div class="large-12 columns">	
 				<div class="panel radius">
-					As Real Estate market hots up Remax 1st class is hiring new realty agents and individuals without
-					broker licence. For licenced brokers we provide:
-					<ul>
-					  	<li>a convenient working place in our office or a separate room</li>
-					  	<li>PC with internet access, telephone number, access to printer, office manager, office parking place, etc.</li>
-					  	<li>Agent website and access to mahor Real Estate property databases </li>
-					  	<li>Friendly office environment and consultation from our expirienced agent</li>
-					  	<li>Higher than average Real Estate Agent Salary. Every 3 days we have a closing in our office, no other company in North Chicagoland and North Suburbs sells/rents more Real Estate property </li>
-					 </ul>
-					 For individuals who decide to become a Real Estate agents we offer support in Broker Licence exam preparation. Since 05.01.2011 Salesperson exam has been substituted by Real Estate Broker licence
-					 exam. Remax 1st class has an extensive office library for Broker Exam preparation. In addition, as a Remax 
-					 representative we often provide no fee service for low-income families. In those cases, Remax 1st class collaborators, who are preparing for Broker exam have the possibility to practice in Real Estate Services.
-					
-					<p> Please send CVs to <a href="mailto:remax1stclass@gmail.com">remax1stclass@gmail.com</a> or call 847-674-9797. </p>
+					<p>As Real Estate market hots up Remax 1st class is hiring new realty agents. For licenced brokers we provide:</p>
+						<ul class="ulWorkWith">
+							<li>a convenient working place in our office or a separate room</li>
+							<li>PC with internet access, office telephone number, parking space, etc.</li>
+							<li>access to mahor Real Estate property databases</li>
+							<li>Friendly office environment and consultation from our expirienced agent</li>
+							<li>Higher than average Real Estate Agent Salary. Every 3 days we have a closing in our office, no other company in North Chicagoland and North Suburbs sells/rents more Real Estate property </li>
+						</ul>
+						<p>For individuals who decide to become a Real Estate agents we offer support in Broker Licence exam preparation. Since 05.01.2011 Salesperson exam has been substituted by Real Estate Broker licence
+							exam. Remax 1st class has an extensive office library for Broker Exam preparation. In addition, as a Remax 
+							representative we often provide no fee service for low-income families. In those cases, Remax 1st class collaborators, who are preparing for Broker exam have the possibility to practice in Real Estate Services.
 
-				</div>
-			</div>		
-		</div>
+						</p>					<p> Please send CVs to <a href="mailto:remax1stclass@gmail.com">remax1stclass@gmail.com</a> or call 847-674-9797. </p>
+
+					</div>
+				</div>		
+			</div>
 		</div>
 	</div>
 </div>
